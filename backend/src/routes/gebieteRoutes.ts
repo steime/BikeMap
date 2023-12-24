567f9100246985f57af64d2d848969b12a2393b0
steime <dominiksteinmann@bluewin.ch>
2023-12-24 15:12:42 +0100

    implement missing endpoints for gebiete

--- backend/src/routes/gebieteRoutes.ts
+++ backend/src/routes/gebieteRoutes.ts
@@ -14,6 +14,22 @@ gebieteRouter.get('/gebiet', async (req: Request, res: Response) => {
     }
 });
 
+gebieteRouter.get('/gebiet/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const gebiet = await Gebiet.findByPk(id);
+
+        if (gebiet) {
+            res.json(gebiet);
+        } else {
+            res.status(404).send('Gebiet not found');
+        }
+    } catch (error) {
+        console.error('Error fetching Gebiet by ID:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
 gebieteRouter.post('/gebiet', async (req: Request, res: Response) => {
     try {
         const { name, lastUpdate, openingHours, numberOfAnlagen, xCoordinate, yCoordinate, website, region, rating, price } = req.body;
@@ -23,6 +39,45 @@ gebieteRouter.post('/gebiet', async (req: Request, res: Response) => {
         res.status(201).json(newGebiet);
     } catch (error) {
         console.error('Error in POST /gebiete:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+gebieteRouter.put('/gebiet/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+
+        const [updatedRows] = await Gebiet.update(updatedData, {
+            where: { id: id }
+        });
+
+        if (updatedRows > 0) {
+            const updatedGebiet = await Gebiet.findByPk(id);
+            res.json(updatedGebiet);
+        } else {
+            res.status(404).send('Gebiet not found');
+        }
+    } catch (error) {
+        console.error('Error updating Gebiet:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+gebieteRouter.delete('/gebiet/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const deleted = await Gebiet.destroy({
+            where: { id: id }
+        });
+
+        if (deleted) {
+            res.status(200).send('Gebiet deleted');
+        } else {
+            res.status(404).send('Gebiet not found');
+        }
+    } catch (error) {
+        console.error('Error deleting Gebiet:', error);
         res.status(500).json({ error: error.message });
     }
 });
